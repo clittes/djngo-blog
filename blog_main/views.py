f97f1e7d49da18aa54ae8b7245decdbaac7c0d8d
clintojoshy <clintojoshyc@gmail.com>
2026-05-13 10:58:15 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render,redirect
 
 from django.http import HttpResponse
 
+from blogs.models import Category,Blog
 
 def home(request):
-    return render (request,'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True,status = 'Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured = False,status = 'Published')
+    context = {
+       
+        'featured_posts':featured_posts,
+        'posts':posts,
+    }
+    return render(request,'home.html',context)
+
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from gc import get_objects
+
+
+
+from . models import Blog, Category
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
+
+def posts_by_category(request,category_id):
+    posts = Blog.objects.filter(status='Published',category= category_id)
+
+    category = get_object_or_404(Category,pk=category_id)
+    # try:
+    #     category = Category.objects.get(pk = category_id)
+    # except:
+    #     return redirect('home')
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html',context)
